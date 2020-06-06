e800a3e94094b974dd752085cf35d03945ac0b60
dominika.lisiecka <dominika.lisiecka@MacBook-Pro-dominikalisiecka.local>
2020-06-07 00:20:59 +0200

    code refactor

--- src/test_new.js
+++ src/test_new.js
@@ -1,9 +1,7 @@
 /*jshint esversion: 6*/
 var x = 'X_VALUE';
-<<<<<<< HEAD
+var k = 'K_ISEVERYTHING';
 var z = 'Z_VALUE';
-var k = 'K_ISEVERYTHING'
-=======
 const fn = ({ a, b }) => {
     const a_1 = a * 2;
     let c;
@@ -16,6 +14,3 @@ const fn = ({ a, b }) => {
     };
     let b_1 = b * 2
 }
-
-var z = 'Z_VALUE';
->>>>>>> d37097196328e986fdd6c63e2d92d56da899ba21
