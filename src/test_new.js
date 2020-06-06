c35975a71c95f4fba86f371bac5db69be45b2026
Maksymilian Hnatejko <maksymilian.hnatejko@gmail.com>
2020-06-06 23:55:54 +0200

    add markup and stylesheet

--- src/test_new.js
+++ src/test_new.js
@@ -1,1 +1,14 @@
+/*jshint esversion: 6*/
 var x = 'X_VALUE';
+const fn = ({ a, b }) => {
+    const a_1 = a * 2;
+    let c;
+    if(b > 0){
+        b = 1;
+    }else{
+        for(let i = 0; i < b; i++){
+            c += i;
+        }
+    };
+    let b_1 = b * 2
+}
